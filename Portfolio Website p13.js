deee35c2be7cdc2b0847d6f910b7949273bc55b5
Mayuresh Satarke <mayureshsattarke@gmail.com>
2024-10-05 12:33:32 +0530

    Add files via upload

---
+++ Portfolio Website p13.js
@@ -0,0 +1,10 @@
+// Add smooth scrolling for nav links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
